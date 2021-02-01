e1f3dcdd9bad1b9072422962bdd95be0951f513c
HMTools <m4t4ngm@gmail.com>
2021-02-01 14:23:34 +0200

    Add Switch From Local To Remote Generating and java envVar checking

--- ViewModels/Main/UMLViewModel.cs
+++ ViewModels/Main/UMLViewModel.cs
@@ -73,7 +73,7 @@ namespace UMLGenerator.ViewModels.Main
         #endregion
         #region Fields
         private CancellationTokenSource cancellationTokenSource;
-        private MainViewModel mainVM;
+        private readonly MainViewModel mainVM;
         private bool isJavaInstalled;
         #endregion
 
@@ -96,9 +96,16 @@ namespace UMLGenerator.ViewModels.Main
                 System.Windows.Clipboard.SetText(PlantUml);
                 mainVM.SetStatus("Copy PlantUML To Clipboard | Succeed", System.Windows.Media.Brushes.Blue, 2000);
             });
-            SwitchLocalOrRemoteCommand = new RelayCommand(o => IsLocal = !IsLocal, o => isJavaInstalled);
+            SwitchLocalOrRemoteCommand = new RelayCommand(o => 
+            {
+                if (isJavaInstalled)
+                    IsLocal = !IsLocal;
+                else
+                    System.Windows.MessageBox.Show
+                    ("Changing to local UML generating Failed : Java isn't installed or ain't registered in the environment variables!!");
+            });
         }
-        public async void UpdateUML(string plantUML)
+        public void UpdateUML(string plantUML)
         {
             ResetUMLProperties(plantUML);
             IsLoading = true;
@@ -107,12 +114,13 @@ namespace UMLGenerator.ViewModels.Main
         }
         private bool CheckIsJavaInstalled()
         {
+            var javaEnvironmentVar = Environment.GetEnvironmentVariable("JAVA_HOME");
             var key32 = Microsoft.Win32.RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.LocalMachine, Microsoft.Win32.RegistryView.Registry32)
                 .OpenSubKey("SOFTWARE\\JavaSoft\\Java Runtime Environment");
             var key64 = Microsoft.Win32.RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.LocalMachine, Microsoft.Win32.RegistryView.Registry64)
                 .OpenSubKey("SOFTWARE\\JavaSoft\\Java Runtime Environment");
 
-            return key32 != null || key64 != null;
+            return (key32 != null || key64 != null) && javaEnvironmentVar != null;
         }
         private void ResetUMLProperties(string plantUML)
         {
@@ -128,34 +136,36 @@ namespace UMLGenerator.ViewModels.Main
         }
         private void SetUml()
         {
-            try
+            Task.Run(() => 
             {
-                if(IsLocal)
+                try
                 {
-                    Task.Run(async () => {
-                        SvgString = await Libraries.PlantUMLMethods.GetLocalSVG(PlantUml, cancellationTokenSource.Token); 
-                    });
-                    UMLImage = Libraries.PlantUMLMethods.GetLocalPNG(PlantUml, cancellationTokenSource.Token);
-                    ImageSource = UMLImage.BitmapToImageSource(System.Drawing.Imaging.ImageFormat.Png);
-                }
-                else
-                {
-                    SvgString =  Libraries.PlantUMLMethods.GetRemoteSVG(PlantUml, cancellationTokenSource.Token).GetAwaiter().GetResult();
-                    Task.Run(() =>
+                    if (IsLocal)
                     {
+                        Task.Run(async () => {
+                            SvgString = await Libraries.PlantUMLMethods.GetLocalSVG(PlantUml, cancellationTokenSource.Token);
+                        });
+
+                        UMLImage = Libraries.PlantUMLMethods.GetLocalPNG(PlantUml, cancellationTokenSource.Token);
+                        ImageSource = UMLImage.BitmapToImageSource(System.Drawing.Imaging.ImageFormat.Png);
+                    }
+                    else
+                    {
+                        SvgString = Libraries.PlantUMLMethods.GetRemoteSVG(PlantUml, cancellationTokenSource.Token).GetAwaiter().GetResult();
                         var svg = Svg.SvgDocument.FromSvg<Svg.SvgDocument>(SvgString);
                         System.Windows.Threading.Dispatcher.CurrentDispatcher.Invoke(() =>
                         {
                             UMLImage = svg.Draw();
                             ImageSource = UMLImage.BitmapToImageSource(System.Drawing.Imaging.ImageFormat.Png);
                         });
-                    });
+                    }
                 }
-            }
-            catch (Exception exception)
-            {
-                Message = $"Failed Getting UML | {exception.Message}";
-            }
+                catch (Exception exception)
+                {
+                    Message = $"Failed Getting UML | {exception.Message}";
+                }
+            });
+            
         }
         #endregion
     }
